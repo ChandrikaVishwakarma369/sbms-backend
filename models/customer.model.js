d4963e9a8808cbd72f12bb7cf7757a2bd86fd8c4
nandiniparmar9691 <nandiniparmar9691@gmail.com>
2026-04-08 14:54:49 +0530

    id storage technique changed

--- models/customer.model.js
+++ models/customer.model.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const customerSchema = new mongoose.Schema(
   {
+    _id: {
+      type: String,
+      default: () => new mongoose.Types.ObjectId().toString(),
+    },
     name: {
       type: String,
       required: [true, "Customer name is required"],
