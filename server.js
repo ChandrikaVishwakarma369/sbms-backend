23457eb24b46cd5ad00e6a32f5135ccaa3bcdc5f
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-06-23 13:16:12 +0530

    url changed

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ const app = express();
 
 
 app.use(cors({
- origin: "http://localhost:5173",
+   origin: [
+    "http://localhost:5173",
+    "https://sbms-gold.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
