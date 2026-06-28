083719c08343f803aba9beb64c1644906d9fb235
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-06-28 14:19:12 +0530

    origins changed

--- server.js
+++ server.js
@@ -28,18 +28,15 @@ connectDB();
 const app = express();
 
 
-// app.use(cors({
-//    origin: [
-//     "http://localhost:5173",
-//     "https://sbms-gold.vercel.app"
-//   ],
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// }));
 app.use(cors({
-  origin: true,
-  credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://sbms-gold.vercel.app",
+    "https://www.sbms-gold.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
