9c21c0b7ee1b6ef3bad655da59fba8d6818e6be8
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-06-28 13:51:02 +0530

    cors err resolved

--- server.js
+++ server.js
@@ -28,14 +28,18 @@ connectDB();
 const app = express();
 
 
+// app.use(cors({
+//    origin: [
+//     "http://localhost:5173",
+//     "https://sbms-gold.vercel.app"
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// }));
 app.use(cors({
-   origin: [
-    "http://localhost:5173",
-    "https://sbms-gold.vercel.app"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: true,
+  credentials: true
 }));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
